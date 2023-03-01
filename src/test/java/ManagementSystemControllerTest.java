1c917ae0e7f63689530e04e9e5c320e83bbda53f
Sezer Lucian Turkmen <74083623+sezerlucianturkmen@users.noreply.github.com>
2023-03-01 23:47:20 +0300

    sonar reviewed

---
+++ src/test/java/ManagementSystemControllerTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagementSystemControllerTest {
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void testAddBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void addBorrower() {
+    }
+
+    @Test
+    void getBorrower() {
+    }
+
+    @Test
+    void deleteBorrower() {
+    }
+
+    @Test
+    void borrowBook() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void findAllBooks() {
+    }
+
+    @Test
+    void findAllBorrowers() {
+    }
+}
